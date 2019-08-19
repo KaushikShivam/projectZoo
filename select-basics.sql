84e336f87903b4581920bd7fc858873572cdeb33
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-08-19 23:27:30 +0530

    Finish Select basics

--- select-basics.sql
+++ select-basics.sql
@@ -1,13 +1,11 @@
---
 -- 1.
+--
 SELECT
-  name,
   population
 FROM
   world
 WHERE
-  population BETWEEN 1000000
-  AND 1250000;
+  name = 'Germany';
 -- 2.
   --
 SELECT
@@ -16,52 +14,14 @@ SELECT
 FROM
   world
 WHERE
-  name LIKE "Al%";
+  name IN ('Sweden', 'Norway', 'Denmark');
 -- 3.
   --
 SELECT
-  name
-FROM
-  world
-WHERE
-  name LIKE '%a'
-  OR name LIKE '%l';
--- 4.
-  --
-SELECT
   name,
-  length(name)
+  area
 FROM
   world
 WHERE
-  length(name) = 5
-  and region = 'Europe';
--- 5.
-  --
-SELECT
-  name,
-  area * 2
-FROM
-  world
-WHERE
-  population = 64000;
--- 6.
-  --
-SELECT
-  name,
-  area,
-  population
-FROM
-  world
-WHERE
-  area > 50000
-  AND population < 10000000;
--- 7.
-  --
-SELECT
-  name,
-  population / area
-FROM
-  world
-WHERE
-  name IN ('China', 'Nigeria', 'France', 'Australia');
+  area BETWEEN 200000
+  AND 250000;
